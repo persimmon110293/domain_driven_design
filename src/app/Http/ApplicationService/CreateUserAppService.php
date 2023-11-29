a90737588db80f75a288b70ca9c8d47e56d22f07
Shohei Kakimoto <shohei.11293@gmail.com>
2023-11-29 21:35:15 +0900

    :up: Updated: feautre/#2 added larastan

--- src/app/Http/ApplicationService/CreateUserAppService.php
+++ src/app/Http/ApplicationService/CreateUserAppService.php
@@ -4,18 +4,15 @@ namespace App\Http\ApplicationService;
 
 use App\Http\Domain\DomainServices\CreateUserDomainService;
 use App\Http\ApplicationService\ICreateUserAppService;
-use App\Http\Repositories\IUserRepository;
 use Illuminate\Http\Request;
 
 class CreateUserAppService implements ICreateUserAppService
 {
     private $domainService;
-    private $repository;
 
-    public function __construct(CreateUserDomainService $createUserDomainService, IUserRepository $userRepository)
+    public function __construct(CreateUserDomainService $createUserDomainService)
     {
         $this->domainService = $createUserDomainService;
-        $this->repository = $userRepository;
     }
 
     public function createUser(Request $request): array
--- src/app/Http/Repositories/UserRepository.php
+++ src/app/Http/Repositories/UserRepository.php
@@ -6,20 +6,8 @@ use App\Http\Repositories\IUserRepository;
 
 class UserRepository implements IUserRepository
 {
-    public function getUserById($id)
-    {
-    }
-
     public function createUser($data)
     {
         // TODO: Implement logic to create a new user in the database
-    }
-
-    public function updateUser($id, $data)
-    {
-    }
-
-    public function deleteUser($id)
-    {
     }
 }

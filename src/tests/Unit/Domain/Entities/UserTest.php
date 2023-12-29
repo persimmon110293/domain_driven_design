546fe809a20f7464d89e52e0bb74849e3381643d
Shohei Kakimoto <shohei.11293@gmail.com>
2023-12-29 16:07:18 +0900

    :rotating_light: Test: feature/#4 added unit test for User.php

---
+++ src/tests/Unit/Domain/Entities/UserTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Http\Domain\Entities\User;
+
+class UserTest extends TestCase
+{
+    private $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $user = new \stdClass();
+        $user->id = '1';
+        $user->name = 'Test User';
+        $user->email = 'test@example.com';
+        $user->password = 'password';
+
+        $this->user = new User($user);
+    }
+
+    public function testId(): void
+    {
+        $this->assertEquals('1', $this->user->id());
+    }
+
+    public function testName(): void
+    {
+        $this->assertEquals('Test User', $this->user->name());
+    }
+
+    public function testEmail(): void
+    {
+        $this->assertEquals('test@example.com', $this->user->email());
+    }
+
+    public function testPassword(): void
+    {
+        $this->assertEquals('password', $this->user->password());
+    }
+}
